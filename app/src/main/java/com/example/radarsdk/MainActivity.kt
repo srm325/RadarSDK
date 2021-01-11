932ce50fac2e00586a4e05219a735096b0e77495
SRM <nnguy22@u.rochester.edu>
2021-01-10 20:56:04 -0500

    Initial commit

--- app/src/main/java/com/example/radarsdk/MainActivity.kt
+++ app/src/main/java/com/example/radarsdk/MainActivity.kt
@@ -1,14 +1,38 @@
-package com.example.radarsdk;
+package com.example.radarsdk
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest
+import android.os.Build
+import io.radar.sdk.Radar.initialize
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.example.radarsdk.R
+import io.radar.sdk.Radar
+import io.radar.sdk.RadarTrackingOptions
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // request permissions
+        if (Build.VERSION.SDK_INT >= 23) {
+            val requestCode = 0
+            if (Build.VERSION.SDK_INT >= 29) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION), requestCode)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestCode)
+            }
+        }
 
-public class MainActivity extends AppCompatActivity {
+        // initialize SDK (replace with your publishable API key)
+        initialize(this, "prj_live_pk_4600174fdbdbe3e6de143b3c7f3dec1142bad4af")
+        // start tracking
+        val trackingOptions : RadarTrackingOptions = RadarTrackingOptions.Builder()
+                .priority(RadarTrackingPriority.RESPONSIVENESS)
+                .offline(RadarTrackingOffline.REPLAY_STOPPED)
+                .sync(RadarTrackingSync.ALL)
+                .build()
+        Radar.startTracking(trackingOptions)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/radarsdk/MyRadarReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.radarsdk
+import android.content.Context
+import io.radar.sdk.Radar
+import io.radar.sdk.RadarReceiver
+import io.radar.sdk.model.RadarEvent
+import io.radar.sdk.model.RadarUser
+
+class MyRadarReceiver : RadarReceiver() {
+
+    override fun onEventsReceived(context: Context, events: Array<RadarEvent>, user: RadarUser) {
+        events.forEach { event ->
+            if (event.type == RadarEvent.RadarEventType.USER_ENTERED_GEOFENCE && event.geofence?.tag == "building" && event.geofence?.externalId == "library") {
+                // user entered library, show notification
+            }
+        }
+    }
+
+}
